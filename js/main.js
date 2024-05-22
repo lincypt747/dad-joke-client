82ff3737751bdf1a1d703d6a099b816a15075b79
Lincy Thomas <lincy.thomas@rfsmart.com>
2024-05-21 20:39:23 -0400

    updated url" l

--- js/main.js
+++ js/main.js
@@ -1,25 +1,16 @@
 const btnEl = document.getElementById('jokebtn');
 const jokeEl = document.getElementById('joke');
 
-//APIKey
-const apiKey = secret.API_Key;
 
-//options
-const options = {
-    method: 'GET',
-    headers: {
-        'X-Api-Key': apiKey,
-    }
-}
 //url to get joke
-const url = 'https://api.api-ninjas.com/v1/dadjokes';
+const url = 'https://lt-dad-joke.onrender.com/joke';
 
 btnEl.addEventListener('click', getJoke);
 
 async function getJoke(){
     try {
         jokeEl.innerText = 'Loading...';
-        const response = await fetch(url, options);
+        const response = await fetch(url);
         const jokeJson = await response.json();
         jokeEl.innerText = jokeJson[0].joke;
 
